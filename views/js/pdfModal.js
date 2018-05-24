313a3e5006b2bd63f0cd48bcf2a32839ac76bb11
filmsterdavid <tanner.davide@gmail.com>
2018-05-23 23:34:04 -0600

    PDFs modal

--- views/js/pdfModal.js
+++ views/js/pdfModal.js
@@ -3,11 +3,18 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var instructions = document.getElementById('myImg');
-var modalInst = document.getElementById("instPdf");
-var pkgPdf = '/DesignPortfolio/views/img/TravlerHerosPackage4.pdf';
+var instructions = document.getElementById('myInst');
+var modalInst = document.getElementById("myInstPdf");
+var packaging = document.getElementById('myPkg');
+var modalPkg = document.getElementById("myPkgPdf");
+var instPdf = '/img/Travler-Hero-5-Instructions.pdf';
+var pkgPdf = '/img/TravlerHerosPackage4.pdf';
 var captionText = document.getElementById("caption");
 instructions.onclick = function(){
+    modal.style.display = "block";
+    window.open(instPdf);
+}
+packaging.onclick = function(){
     modal.style.display = "block";
     window.open(pkgPdf);
 }
